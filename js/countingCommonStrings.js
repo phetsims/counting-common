821d3ba1c4bb58b683c4dd137b73b5ade0b1340e
chrisklus <christopher.klusendorf@colorado.edu>
2021-06-09 21:18:46 -0600

    Add strings js file for counting-common, see https://github.com/phetsims/number-play/issues/19

---
+++ js/countingCommonStrings.js
@@ -0,0 +1,14 @@
+// Copyright 2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+
+import getStringModule from '../../chipper/js/getStringModule.js';
+import countingCommon from './countingCommon.js';
+
+const countingCommonStrings = getStringModule( 'COUNTING_COMMON' );
+
+countingCommon.register( 'countingCommonStrings', countingCommonStrings );
+
+export default countingCommonStrings;
