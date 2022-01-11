6fe0cbd587cb801c6a964e2b6ab08cf4b1498279
Luisav1 <luisa.vargassuarez@ucalgary.ca>
2022-01-11 10:58:57 -0700

    Convert BaseNumberNode.ts contents to TypeScript. See https://github.com/phetsims/number-play/issues/71.

--- js/common/view/BaseNumberNode.ts
+++ js/common/view/BaseNumberNode.ts
@@ -28,9 +28,23 @@ import paperBackground100_png from '../../../mipmaps/paperBackground100_png.js';
 import paperBackground10_png from '../../../mipmaps/paperBackground10_png.js';
 import paperBackground1_png from '../../../mipmaps/paperBackground1_png.js';
 import countingCommon from '../../countingCommon.js';
+import BaseNumber from '../model/BaseNumber.js';
+
+// types
+type XorYoffset = {
+  [ key: number ]: number
+};
+
+type ZeroOffset = {
+  [ key: number ]: number[]
+}
+
+type ImageMap = {
+  [ key: number ]: any // TODO-TS: Figure out the type for mipmaps
+};
 
 // place => mipmap info
-const BACKGROUND_IMAGE_MAP = {
+const BACKGROUND_IMAGE_MAP: ImageMap = {
   0: paperBackground1_png,
   1: paperBackground10_png,
   2: paperBackground100_png,
@@ -38,7 +52,7 @@ const BACKGROUND_IMAGE_MAP = {
 };
 
 // digit => mipmap info
-const DIGIT_IMAGE_MAP = {
+const DIGIT_IMAGE_MAP: ImageMap = {
   1: digit1_png,
   2: digit2_png,
   3: digit3_png,
@@ -51,21 +65,21 @@ const DIGIT_IMAGE_MAP = {
 };
 
 // place => x/y offsets for the first digit in each place
-const PLACE_X_OFFSET = { 0: 64, 1: 62, 2: 70, 3: 94 };
-const PLACE_Y_OFFSET = { 0: 38, 1: 61, 2: 82, 3: 104 };
+const PLACE_X_OFFSET: XorYoffset = { 0: 64, 1: 62, 2: 70, 3: 94 };
+const PLACE_Y_OFFSET: XorYoffset = { 0: 38, 1: 61, 2: 82, 3: 104 };
 
 // place => x/y offsets for the handle in each place
-const PLACE_HANDLE_X_OFFSET = { 0: 130, 1: 142, 2: 151, 3: 171 };
-const PLACE_HANDLE_Y_OFFSET = { 0: 97, 1: 74, 2: 53, 3: 31 };
+const PLACE_HANDLE_X_OFFSET: XorYoffset = { 0: 130, 1: 142, 2: 151, 3: 171 };
+const PLACE_HANDLE_Y_OFFSET: XorYoffset = { 0: 97, 1: 74, 2: 53, 3: 31 };
 
 // digit => horizontal offset for that digit (applied to all places, includes digit-specific information)
-const DIGIT_X_OFFSET = { 1: 93, 2: -7, 3: -7, 4: -9, 5: -18, 6: -5, 7: -24, 8: -2, 9: -10 };
+const DIGIT_X_OFFSET: XorYoffset = { 1: 93, 2: -7, 3: -7, 4: -9, 5: -18, 6: -5, 7: -24, 8: -2, 9: -10 };
 
 // digit => horizontal offset, customized for each single digit base number
-const FIRST_PLACE_DIGIT_X_OFFSET = { 1: -61, 2: 0, 3: 0, 4: 0, 5: 5, 6: 0, 7: 15, 8: 10, 9: 15 };
+const FIRST_PLACE_DIGIT_X_OFFSET: XorYoffset = { 1: -61, 2: 0, 3: 0, 4: 0, 5: 5, 6: 0, 7: 15, 8: 10, 9: 15 };
 
 // place => horizontal positions of the zeros in the base number
-const ZERO_OFFSET = {
+const ZERO_OFFSET: ZeroOffset = {
   0: [],
   1: [ 272 ],
   2: [ 530, 284 ],
@@ -76,16 +90,13 @@ const ZERO_OFFSET = {
 const SCALE = 72 / 300;
 
 class BaseNumberNode extends Node {
-  /**
-   * @param {BaseNumber} baseNumber
-   * @param {number} opacity
-   * TODO: refactor as options? https://github.com/phetsims/counting-common/issues/1
-   * @param {boolean} includeHandles
-   * @param {boolean} isLargestBaseNumber
-   * @param {boolean} hasDescendant
-   * @param {boolean} isPartOfStack
-   */
-  constructor( baseNumber, opacity, includeHandles, isLargestBaseNumber, hasDescendant, isPartOfStack ) {
+
+  public static PAPER_NUMBER_DIMENSIONS: Object;
+  public static IMAGE_OFFSETS: Vector2[];
+  public readonly handleStemNode: Path | undefined;
+
+  // TODO: refactor as options? https://github.com/phetsims/counting-common/issues/1
+  constructor( baseNumber: BaseNumber, opacity: number, includeHandles: boolean, isLargestBaseNumber: boolean, hasDescendant: boolean, isPartOfStack: boolean ) {
     super( { scale: SCALE } );
 
     // Position of the initial digit
@@ -112,14 +123,13 @@ class BaseNumberNode extends Node {
     // get a handle because grabbing it would pull 1000 away from 200, and this can be accomplished by pulling the 200
     // off instead. in the case of 2200, the first 2 should get a handle because grabbing it would pull off one 1000.
     if ( includeHandles && !( baseNumber.numberValue === 1 && !isPartOfStack )
-    && !( isLargestBaseNumber && baseNumber.digit === 1 && hasDescendant ) ) {
+         && !( isLargestBaseNumber && baseNumber.digit === 1 && hasDescendant ) ) {
 
       const lineWidth = 6;
 
       // The handle that attaches to the paper
       const handleStemShape = new Shape().moveTo( 0, 0 ).lineTo( 0, PLACE_HANDLE_Y_OFFSET[ baseNumber.place ] );
 
-      // @public (read-only)
       this.handleStemNode = new Path( handleStemShape, {
         stroke: 'black',
         lineWidth: lineWidth
@@ -172,13 +182,13 @@ class BaseNumberNode extends Node {
 }
 
 /**
- * @public {Object} - Maps place (0-3) to a {Dimension2} with the width/height
+ * Maps place (0-3) to a {Dimension2} with the width/height
  */
 BaseNumberNode.PAPER_NUMBER_DIMENSIONS = _.mapValues( BACKGROUND_IMAGE_MAP, mipmap => new Dimension2( mipmap[ 0 ].width * SCALE, mipmap[ 0 ].height * SCALE ) );
 
 /**
- * @public {Array.<Vector2>} - Maps place (0-3) to a {Vector2} that is the offset of the upper-left corner of the
- *                             BaseNumberNode relative to a 1-digit BaseNumberNode.
+ * Maps place (0-3) to a {Vector2} that is the offset of the upper-left corner of the BaseNumberNode relative to a
+ * 1-digit BaseNumberNode.
  */
 BaseNumberNode.IMAGE_OFFSETS = [
   new Vector2( -14, 0 ),
