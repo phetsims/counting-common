af8989be0d8bd46c039e9edee276284e8153bb5b
pixelzoom <cmalley@pixelzoom.com>
2023-01-09 13:07:37 -0700

    fix comment typo

--- js/common/model/CountingObject.ts
+++ js/common/model/CountingObject.ts
@@ -218,7 +218,7 @@ class CountingObject {
   }
 
   /**
-   * If our paper number is outside of the available view bounds, move in inside those bounds.
+   * If our paper number is outside of the available view bounds, move it inside those bounds.
    *
    * @param viewBounds
    * @param newDestination
