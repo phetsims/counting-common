d2d508974449d9e7acc8e0e7511e6a9c533af311
phet-dev <phetadmin@gmail.com>
2023-01-10 04:17:57 -0700

    update copyright dates from daily grunt work

--- js/common/CountingCommonConstants.ts
+++ js/common/CountingCommonConstants.ts
@@ -1,4 +1,4 @@
-// Copyright 2021-2022, University of Colorado Boulder
+// Copyright 2021-2023, University of Colorado Boulder
 
 /**
  * Constants that are shared between the various portions of Counting Common code
